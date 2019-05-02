07325a6cf42458dad0adeeab2e05015609058040
Raymond Loman <raymond@carpago.nl>
2019-05-02 11:09:10 +0200

    Add demo code for static inner class

---
+++ src/main/java/nl/capgemini/diving/staticinnerclass/Application.java
@@ -0,0 +1,14 @@
+package nl.capgemini.diving.staticinnerclass;
+
+public class Application {
+    public static void main(String[] args) {
+
+//        ChargingPole.ChargingPoleBuilder builder = new ChargingPole.ChargingPoleBuilder("ChaDemo");
+
+        ChargingPole pole = new ChargingPole.ChargingPoleBuilder("ChaDemo").location("Utrecht").numberOfChargers(3).build();
+
+
+
+
+    }
+}
