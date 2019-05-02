8c39a0384957c7161184fe1243694b3e7131ddfe
Raymond Loman <raymond@carpago.nl>
2019-05-02 22:45:28 +0200

    Optimize imports

--- src/main/java/nl/capgemini/diving/hashCodeEquals/equalstrainer/Application.java
+++ src/main/java/nl/capgemini/diving/hashCodeEquals/equalstrainer/Application.java
@@ -2,7 +2,10 @@ package nl.capgemini.diving.hashCodeEquals.equalstrainer;
 
 import nl.capgemini.diving.model.Suit;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Application {
 
