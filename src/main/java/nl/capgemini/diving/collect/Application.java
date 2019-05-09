c3feca719d29880c8e57570d5ea8ad57ae0317b3
Raymond Loman <raymond@carpago.nl>
2019-05-09 21:25:14 +0200

    Add demo for collect

---
+++ src/main/java/nl/capgemini/diving/collect/Application.java
@@ -0,0 +1,22 @@
+package nl.capgemini.diving.collect;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Application {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Mukesh", "Vishal", "Amar");
+
+        String result = list.parallelStream().collect(
+                StringBuilder::new, // Supplier
+                (response, element) -> response.append(" ").append(element), // accumulator
+                (response1, response2) -> response1.append(",").append(response2.toString())) // combiner
+                .toString();
+        System.out.println("Result: " + result);
+    }
+
+    // show the stream::collect(Collectors.toList())
+}
