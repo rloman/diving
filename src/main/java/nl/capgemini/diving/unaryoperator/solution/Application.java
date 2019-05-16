633b5db0fa4c56017c519b5ac49394056b3ad99a
Raymond Loman <raymond@carpago.nl>
2019-05-16 21:26:01 +0200

    Add file

---
+++ src/main/java/nl/capgemini/diving/unaryoperator/solution/Application.java
@@ -0,0 +1,12 @@
+package nl.capgemini.diving.unaryoperator.solution;
+
+import nl.capgemini.diving.methodreferences.Mask;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Mask m = new Mask();
+
+    }
+}
