4f71cdfe44dbbdee74fe4271976775591ace3053
Raymond Loman <raymond@carpago.nl>
2019-05-23 14:25:42 +0200

    Translate Hanoi to English

--- src/main/java/nl/capgemini/diving/hanoi/Hanoi.java
+++ src/main/java/nl/capgemini/diving/hanoi/Hanoi.java
@@ -2,20 +2,20 @@ package nl.capgemini.diving.hanoi;
 
 public class Hanoi {
 
-    public void hanoi(int aantal) {
-        hanoi(aantal, 1, 2, 3);
+    public void hanoi(int numberOfDices) {
+        hanoi(numberOfDices, 1, 2, 3);
     }
 
 
-    private void hanoi(int aantal, int van, int via, int naar) {
+    private void hanoi(int numberOfDices, int from, int using, int destination) {
 
 
-        if (aantal == 0) {
+        if (numberOfDices == 0) {
             return;
         } else {
-            hanoi(aantal - 1, van, naar, via);
-            System.out.println("Move " + van + " naar " + naar);
-            hanoi(aantal - 1, via, van, naar);
+            hanoi(numberOfDices - 1, from, destination, using);
+            System.out.println("Move " + from + " to " + destination);
+            hanoi(numberOfDices - 1, using, from, destination);
         }
 
     }
